0a8945b5c00517e0e686a7c09895a518fb569a19
琴梨梨 <yeyaohua2010@163.com>
2021-08-13 17:08:50 +0800

    1.0.

--- SakiProgress.js
+++ SakiProgress.js
@@ -7,7 +7,7 @@ var SakiProgress = {
     init: function (color) {
         if (!this.isLoaded) {
             this.isLoaded = true;
-            console.info("SakiProgress Initializing!\nVersion:1.0.1\nQinlili Tech:Github@qinlili23333");
+            console.info("SakiProgress Initializing!\nVersion:1.0.2\nQinlili Tech:Github@qinlili23333");
             this.pgDiv = document.createElement("div");
             this.pgDiv.id = "pgdiv";
             this.pgDiv.style = "z-index:9999;position:fixed;background-color:white;min-height:32px;width:auto;height:32px;left:0px;right:0px;top:0px;box-shadow:0px 2px 2px 1px rgba(0, 0, 0, 0.5);transition:opacity 0.5s;display:none;";
@@ -17,16 +17,36 @@ var SakiProgress = {
             this.first.style.transition = "margin-top 0.5s"
             this.progress = document.createElement("div");
             this.progress.id = "dlprogress"
-            this.progress.style = "position: absolute;top: 0;bottom: 0;left: 0;background-color: #F17C67;z-index: -1;width:0%;transition: width 1s ease-in-out,opacity 0.25s;"
+            this.progress.style = "position: absolute;top: 0;bottom: 0;left: 0;background-color: #F17C67;z-index: -1;width:0%;transition: width 1s ease-in-out,opacity 0.25s,background-color 1s;"
             if (color) {
-                this.progress.style.backgroundColor = color;
+                this.setColor(color);
             }
             this.pgDiv.appendChild(this.progress);
             this.textSpan = document.createElement("span");
             this.textSpan.style = "height:32px;padding:4px;font-size:24px;";
+            this.textSpan.style.display = "inline-block"
             this.pgDiv.appendChild(this.textSpan);
+            var css = ".barBtn:hover{ background-color: #cccccc }.barBtn:active{ background-color: #999999 }";
+            var style = document.createElement('style');
+            if (style.styleSheet) {
+                style.styleSheet.cssText = css;
+            } else {
+                style.appendChild(document.createTextNode(css));
+            }
+            document.getElementsByTagName('head')[0].appendChild(style);
         } else {
             console.error("Multi Instance Error-SakiProgress Already Loaded!");
+        }
+    },
+    destroy: function () {
+        if (this.pgDiv) {
+            document.body.removeChild(this.pgDiv);
+            this.isLoaded = false;
+            this.progres = false;
+            this.pgDiv = false;
+            this.textSpan = false;
+            this.first = false;
+            console.info("SakiProgress Destroyed!You Can Reload Later!");
         }
     },
     setPercent: function (percent) {
@@ -75,13 +95,35 @@ var SakiProgress = {
             console.error("Not Initialized Error-Please Call `init` First!");
         }
     },
-    destroy: function () {
-        document.body.removeChild(this.pgDiv);
-        this.isLoaded = false;
-        this.progres = false;
-        this.pgDiv = false;
-        this.textSpan = false;
-        this.first = false;
-        console.info("SakiProgress Destroyed!You Can Reload Later!");
+    setColor: function (color) {
+        if (this.progress) {
+            this.progress.style.backgroundColor = color;
+        }
+        else {
+            console.error("Not Initialized Error-Please Call `init` First!");
+        }
+    },
+    addBtn: function (img) {
+        if (this.pgDiv) {
+            var btn = document.createElement("img");
+            btn.style = "display: inline-block;right:0px;float:right;height:32px;width:32px;transition:background-color 0.2s;"
+            btn.className = "barBtn"
+            btn.src = img;
+            this.pgDiv.appendChild(btn);
+            return btn;
+        }
+        else {
+            console.error("Not Initialized Error-Please Call `init` First!");
+        }
+    },
+    removeBtn: function (btn) {
+        if (this.pgDiv) {
+            if (btn) {
+                this.pgDiv.removeChild(btn);
+            }
+        }
+        else {
+            console.error("Not Initialized Error-Please Call `init` First!");
+        }
     }
 }
